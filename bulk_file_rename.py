77dcd24049718de31f14edb4ecbf6e8b23f75381
shmithu <87830860+shmithu@users.noreply.github.com>
2023-10-11 02:38:47 +0600

    bulk_file_rename.py

--- bulk_file_rename.py
+++ bulk_file_rename.py
@@ -1,18 +1,27 @@
 import os
+if __name__ == "__main__":
+    reset = "\033[0m"
+    cyan = "\033[36m"
+    red = "\033[91m"
+    print(cyan)
+    print("""     ___                                  _     ____
+    |_ _|_ __  _ __   ___   ___ ___ _ __ | |_  | __ )  ___  _   _
+     | || '_ \| '_ \ / _ \ / __/ _ \ '_ \| __| |  _ \ / _ \| | | |
+     | || | | | | | | (_) | (_|  __/ | | | |_  | |_) | (_) | |_| |
+    |___|_| |_|_| |_|\___/ \___\___|_| |_|\__| |____/ \___/ \__, |
+                                     Nothing is Impossible  |___/""")
+    print(reset)
+    directory = input("Enter the Directory Path [:E:/] :")
+    i=1
+    for filename in os.listdir(directory):
+        if filename:
+            base_name, extension = os.path.splitext(filename)
+            new_name = str(i)+"" + extension
+            old_path = os.path.join(directory, filename)
+            new_path = os.path.join(directory, new_name)
 
-import condition as condition
-
-directory = input("Enter the Directory Path [:E:/Tomato/] :")
-i=1
-for filename in os.listdir(directory):
-    if filename:
-        base_name, extension = os.path.splitext(filename)
-        new_name = str(i)+"" + extension
-        old_path = os.path.join(directory, filename)
-        new_path = os.path.join(directory, new_name)
-
-        os.rename(old_path, new_path)
-        print(old_path,"------> ",new_path)
-        i+=1
-print("File renaming complete.")
+            os.rename(old_path, new_path)
+            print(old_path,"------> ",new_path)
+            i+=1
+    print(red,"File renaming complete.")
 
